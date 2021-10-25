1ee718c09b3f1fc03512eb8ab73859e1275f5b59
eledoro <lena@booggee.com>
2021-10-25 13:48:27 +0100

    added to GameContext

--- src/contexts/gameContext/index.js
+++ src/contexts/gameContext/index.js
@@ -2,17 +2,19 @@ import React, { useState, useContext } from 'react';
 
 const GameContext = React.createContext();
 
-export function useGame() {
+export function useGameContext() {
     return useContext(GameContext)
 }
 export function GameProvider({ children }){
   const [host, setHost]=useState();
-  const [players, setPlayers] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [gameSettings, setGameSettings] = useState({numberOfQuestions:0, level:"", topic:""});
+  const [roomName, setRoomName]=useState("");
 
 
 
 return (
-    <GameContext.Provider value={roomName, host, playersList, gameSettings}>
+    <GameContext.Provider value={{roomName, host, players, gameSettings, setRoomName, setHost, setPlayers, setGameSettings}}>
         { children }
     </GameContext.Provider>
 )
--- src/pages/CreateRoom/index.js
+++ src/pages/CreateRoom/index.js
@@ -1,19 +1,40 @@
 import React, {useState, useContext} from 'react'
 import { useAuthContext } from '../../contexts/auth'
-
+import { useGameContext } from '../../contexts/gameContext' 
 const CreateRoom = () => {
 
-    //const { currentUser } = useContext(useAuthContext)
+ //const { currentUser } = useContext(useAuthContext)
+ 
+ // ?? currentUser - should be from localStorage or from Auth ??
+    
+    const {setHost, roomName, setRoomName, players, setPlayers} = useGameContext()
 
+function updateRoomName(e) {
+    let newInput = e.target.value;
+    setRoomName(newInput)
+}
+
+function submitNewRoom(e) {
+    e.preventDefault()
+   console.log('room name ', roomName)
+  // setHost(currentUser) 
+}
+
+function joinRoom(e) {
+    e.preventDefault()
+    
+// setPlayers([..players, currentUser])  
+
+}
     return (
 
         <div>
             <h2>Welcome</h2>
 
-            <h3>Enter Room Name: <span><input type = "text"></input></span></h3>
+            <h3>Enter Room Name: <span><input type = "text" onChange={updateRoomName}></input></span></h3>
             <span>
-                <button>New Room</button>
-                <button>Join</button></span>
+                <button onClick={submitNewRoom}  >New Room</button>
+                <button onClock={joinRoom}>Join</button></span>
         </div>
     )
 }
