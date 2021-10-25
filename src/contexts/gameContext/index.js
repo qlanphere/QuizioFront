0470c9d97173c94ae97bb8ec5acfc75d6d15d0ae
eledoro <lena@booggee.com>
2021-10-25 12:00:38 +0100

    draft gameContext

--- src/contexts/gameContext/index.js
+++ src/contexts/gameContext/index.js
@@ -2,9 +2,18 @@ import React, { useState, useContext } from 'react';
 
 const GameContext = React.createContext();
 
+export function useGame() {
+    return useContext(GameContext)
+}
+export function GameProvider({ children }){
+  const [host, setHost]=useState();
+  const [players, setPlayers] = useState("");
+
+
+
 return (
-    <GameContext.Provider value={auth}>
+    <GameContext.Provider value={roomName, host, playersList, gameSettings}>
         { children }
     </GameContext.Provider>
 )
-
+}
