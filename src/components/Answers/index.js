4719a8b521184252b208dc6a9f08c4f42222a50f
eledoro <lena@booggee.com>
2021-10-26 11:47:29 +0100

    answers shuffle

--- src/components/Answers/index.js
+++ src/components/Answers/index.js
@@ -16,7 +16,27 @@ const Answers = (props) => {
      console.log(score)
    }
 
-   const options = allOptions.map( (answ,i) => 
+//  to shuffle the  answers array
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+   const options = shuffle(allOptions).map( (answ,i) => 
    /* <button key={i} onClick={()=>console.log(answ ===questions[props.index].correct_answer)} >{containsEncodedComponents(answ)}</button>)*/
       
     <button key={i} onClick={handleAnswer } >{decodeURIComponent(answ)}</button>)
