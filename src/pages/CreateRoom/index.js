f4d167927b6784d3fca9637ff3b3ebf92ea80487
Quinn Lanphere <qlanphere@yahoo.com>
2021-10-26 10:26:22 +0100

    added checkboxes and socket stuff to front end

--- src/pages/CreateRoom/index.js
+++ src/pages/CreateRoom/index.js
@@ -5,49 +5,50 @@ import {io} from 'socket.io-client'
 const socket = io('http://localhost:8080')
 
 const CreateRoom = () => {
-    
 
+    const [join, setJoin] = useState(true)
+    const [create, setCreate] = useState(false)
     
+const {currentUser} = useAuthContext()
+const {setHost, roomName, setRoomName, players, setPlayers} = useGameContext()
 
-        
+const [message, setMessage] = useState('')
+const centerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: 'center'
+}
+
+function handleJoin () {
+    if (create) {
+        setJoin(true)
+        setCreate(false)
+    } else return
+}
+function handleCreate () {
+    if (join) {
+        setCreate(true)
+        setJoin(false)
+    } else return
+}
+  
         socket.on("connect", () => {
             console.log(`user is connected on ${socket.id}`)
         })
-        socket.on('joined', (id, room) => {
+        socket.on('joined', (user, room) => {
             console.log(`my id is ${socket.id}`)
-            console.log(`${id} has joined room: ${room}`)
+            console.log(`${user} has joined room: ${room}`)
+            setMessage(`${user} has joined room: ${roomName}`)
         })
-        
-    
-
-    
-
- //const { currentUser } = useContext(useAuthContext)
- 
- // ?? currentUser - should be from localStorage or from Auth ??
-    
-    const {setHost, roomName, setRoomName, players, setPlayers} = useGameContext()
-
-// function updateRoomName(e) {
-//     e.preventDefault()
-//     let newInput = e.target.value;
-//     setRoomName(newInput)
-// }
 
 
-// function submitNewRoom(e) {
-//     e.preventDefault()
-//    console.log('room name: ', roomName)
-//   // setHost(currentUser) 
-//   socket.emit('join-room', roomName)
-// }
 
 function joinRoom(e) {
     e.preventDefault()
-    setRoomName(e.target[0].value)
-    console.log(e.target[0].value)
-    console.log(roomName)
-    socket.emit('join-room', roomName, socket.id)
+    let room = e.target[0].value
+    setRoomName(room)
+    console.log(currentUser.name)
+    socket.emit('join-room', room, currentUser.name)
 // setPlayers([..players, currentUser])  
 
 }
@@ -56,11 +57,17 @@ function joinRoom(e) {
         <div>
             <h2>Welcome</h2>
             <form onSubmit = {joinRoom}>
-            <h3>Enter Room Name: <span><input type = "text" ></input></span></h3>
+            <h3>Enter Room Name: <input type = "text" ></input></h3>
             <span>
+                <label for = "join">Join</label>
+                <input style = {{marginRight: '10px', marginLeft: '10px'}}  onChange = {handleJoin} checked = {join} type = "checkbox" for = "join"></input>
+                <label for = "create">Create Room</label>
+                <input style = {{marginRight: '10px', marginLeft: '10px'}} onChange = {handleCreate} checked = {create} type = "checkbox" for = "create"></input>
                 {/* <button  >New Room</button> */}
-                <button type = "submit">Join</button></span>
+                <button type = "submit">Submit</button>
+                </span>
             </form>
+            <div style = {centerStyle}>{message}</div>
         </div>
     )
 }
