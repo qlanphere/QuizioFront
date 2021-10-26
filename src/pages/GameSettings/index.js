866585488c683cce40b7e4bad3e07edfcd7b2fca
Quinn Lanphere <qlanphere@yahoo.com>
2021-10-26 16:20:53 +0100

    added settings options and redirect to lobby

--- src/pages/GameSettings/index.js
+++ src/pages/GameSettings/index.js
@@ -1,18 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import { useAuthContext } from '../../contexts/auth'
 import { useGameContext } from '../../contexts/gameContext' 
 import { useHistory, useParams } from 'react-router-dom';
+import { SocketContext } from '../../contexts/socketContext';
 
 const GameSettings = () =>{
 
 const {host, roomName, gameSettings, setGameSettings, questions, setQuestions} = useGameContext()
+const socket = useContext(SocketContext)
 const history = useHistory();
 
 // -------   for testing  fetch
-const topic = 23  // should be a number
-const numberOfQuestions=4
-const level="medium"
+// const topic = 23  // should be a number
+// const numberOfQuestions=4
+// const level="medium"
 // -----
+
+const trivia_categories = [{"id":9,"name":"General Knowledge"},{"id":10,"name":"Entertainment: Books"},
+{"id":11,"name":"Entertainment: Film"},{"id":12,"name":"Entertainment: Music"},
+{"id":13,"name":"Entertainment: Musicals & Theatres"},{"id":14,"name":"Entertainment: Television"},
+{"id":15,"name":"Entertainment: Video Games"},{"id":16,"name":"Entertainment: Board Games"},
+{"id":17,"name":"Science & Nature"},{"id":18,"name":"Science: Computers"},{"id":19,"name":"Science: Mathematics"},
+{"id":20,"name":"Mythology"},{"id":21,"name":"Sports"},{"id":22,"name":"Geography"},{"id":23,"name":"History"},
+{"id":24,"name":"Politics"},{"id":25,"name":"Art"},{"id":26,"name":"Celebrities"},{"id":27,"name":"Animals"},
+{"id":28,"name":"Vehicles"},{"id":29,"name":"Entertainment: Comics"},{"id":30,"name":"Science: Gadgets"},
+{"id":31,"name":"Entertainment: Japanese Anime & Manga"},{"id":32,"name":"Entertainment: Cartoon & Animations"}]
 
 async function fetchQuiz(numberOfQuestions, level, topic) {
   try {
@@ -29,31 +41,52 @@ async function fetchQuiz(numberOfQuestions, level, topic) {
     }
   }
   
-  
-  
-  function handleRoomSettings(){
-    
-    // set Settings  
-    setGameSettings({numberOfQuestions: numberOfQuestions, level: level, topic: topic})
-    
-    // fetch questions ?? should it be here?
-    
-    fetchQuiz(numberOfQuestions, level, topic)
-    
-    // rederect to the lobby
-history.push(`/lobby/${roomName}`)
+  function handleSubmit(e) {
+    e.preventDefault()
+    let category = e.target[0].value
+    let topic = trivia_categories.filter(e => e.name === category)
+    console.log(topic)
+    let numberOfQuestions = e.target[1].value
+    let difficulty = e.target[2].value
 
-}
+    setGameSettings({topic: topic[0].id, numberOfQuestions: numberOfQuestions, level: difficulty})
+
+    fetchQuiz(numberOfQuestions, difficulty, topic[0].id)
+
+    history.push(`/lobby/${roomName}`)
+  }
+
 return (
     <div id="settings">
         <h2>welcome to  {roomName} </h2>
         <h2> choose settings: </h2>
+        <form onSubmit = {handleSubmit}>
+          <select id = "cateogry"name = "categories">
+            <option value = "General Knowledge">General Knowledge</option>
+            <option value = "Entertainment: Film">Entertainment: Film</option>
+            <option value = "Entertainment: Music">Entertainment: Music</option>
+            <option value = "Entertainment: Television">Entertainement: Television</option>
+            <option value = "Entertainemnt: Video Games">Entertainemnt: Video Games</option>
+            <option value = "Science & Nature">Science & Nature</option>
+            <option value = "Science: Computers">Science: Computers</option>
+            <option value = "Sports">Sports</option>
+            <option value = "Geography">Geography</option>
+            <option value = "History">History</option>
+            <option value = "Politics">Politics</option>
+            <option value = "Science: Gadgets">Science: Gadgets</option>
+          </select>
+          <input type = "text" placeholder = "number of questions"></input>
 
-    
+          <select>
+            <option value = "easy">Easy</option>
+            <option value = "medium">Medium</option>
+            <option value = "hard">Hard</option>
+          </select>
+          <button type = "submit">Submit</button>
+          
 
-        <button onClick={handleRoomSettings}>Submit settings</button>
+        </form>
 
-       
     </div>
 )
 }
