d7085681eb657ffc4648bac157c00c9da93bdfa2
eledoro <lena@booggee.com>
2021-10-28 16:47:59 +0100

    Added leaderboard table

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -4,38 +4,43 @@ import PlayerScoreCard from "../PlayerScoreCard";
 
 
 const Leaderboard = () => {
+    const [data, setData] = useState([])
 
-    const [leaderboard, setLeaderboard] = useState()
-    const [error, setError] = useState();
+    //must be empty array otherwise map not a function
 
-    const url = 'http://localhost:3000/user/leaderboard'
+    useEffect(()=> {
+        if(!data.length){
+            retrieve()
+        }
+    }, [])
 
-// fetch liderboard data 
-async function getLiderboard() {
-    try {
-        const data = await fetch(url)
-        const leaderboard = await data.json();
-        setLeaderboard(leaderboard)
-    } catch (err) {
-        setError(err.message);
+    async function retrieve(){
+        const response = await fetch('http://localhost:3000/user/leaderboard')
+        const data = await response.json() 
+        setData(data)
+        console.log(data)
+    
     }
-}
-
-useEffect(() => { 
-   getLiderboard()
-   console.log("leaderboard", leaderboard)
-},[])
+    
+    const userNames = data.map(user => user.username)
+    const totalScores = data.map(user => user.total_scores)
+    const totalGames = data.map(user => user.total_games)
+    console.log(totalGames)
+    console.log(totalScores)
+    console.log(userNames)
+    
 
 
   //const Lines = leaderboard.map( line => line*2)
   //console.log(Lines)
- // const Lines = leaderboard.map( line => <PlayerScoreCard  props={line} />)
+   const lines = data.map( (line,i) => <p key={i} >{i+1}   {line.username}   {line.total_scores}</p>)
 
  // {leaderboard.map((line,i) => <PlayerScoreCard key={i}  line={line} />)}
     return(
 
     <div> 
         <h2>Leaderboard</h2>
+        {lines}
     
        
     
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import BarChart from '../../components/BarChart'
+import Leaderboard from '../../components/Leaderboard'
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome to Quizio!</h1>
             <h2>Instructions:</h2>
-            <ul>
-                <li>hiifuiehfuiehui</li>
-            </ul>
+<BarChart />
+<Leaderboard />
+            
         </div>
+        
     )
 }
 
