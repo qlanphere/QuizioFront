480dd07209044a890d5dadbf6deb074b87fa9370
mussb00 <berhanem2000@gmail.com>
2021-10-28 20:19:15 +0100

    styling for the login and registration page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { useAuthContext } from "../../contexts/auth";
 import { useHistory } from "react-router-dom";
-
+import PasswordIcon from '@mui/icons-material/Password'
+import EmailIcon from '@mui/icons-material/Email'
+import styles from './Login.module.css'
 const Login = () => {
     const { login } = useAuthContext();
     const history = useHistory();
 
-    const [ formData, setFormData ] = useState({ email: "", password: "" })
-    const [ error, setError ] = useState();
-    const [ loading, setLoading ] = useState(false);
-    
+    const [formData, setFormData] = useState({ email: "", password: "" })
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+
     const handleInput = e => setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }))
     const formIncomplete = () => Object.values(formData).some(v => !v)
 
@@ -24,16 +26,26 @@ const Login = () => {
             setError(err)
         }
     }
-
     return (
         <>
-        <form onSubmit={handleSubmit} aria-label="login">
-            <input type="email" name="email" value={formData.email} onChange={handleInput} placeholder="Email" />
-            <input type="password" name="password" value={formData.password} onChange={handleInput} placeholder="Password" />
-            <input type="submit" className={formIncomplete() ? 'disabled' : 'enabled'} disabled={formIncomplete()} value="Login" />
-        </form>
-        { error && <div id="error">{error}</div> }
-        { loading && <div id="loading">Logging in . . .</div> }
+            <form onSubmit={handleSubmit} aria-label="login">
+                <div className="mt-5">
+                    <div className="m-2">
+                        <EmailIcon color="primary"/>
+                        <input className ={styles.input} type="email" name="email" value={formData.email} onChange={handleInput} placeholder="Email" />
+                    </div>
+                    <div className="m-2">
+                        <PasswordIcon color="primary"/>
+                        <input className ={styles.input} stype="password" name="password" value={formData.password} onChange={handleInput} placeholder="Password" />
+                    </div>
+                    <div className="m-2">
+                        <input type="submit" className={formIncomplete() ? 'disabled' : 'enabled'} disabled={formIncomplete()} value="Login" />
+                    </div>
+                </div>
+
+            </form>
+            {error && <div id="error">{error}</div>}
+            {loading && <div id="loading">Logging in . . .</div>}
         </>
     );
 }
