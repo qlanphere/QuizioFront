e8681841dc01525fefcd0f8511542f8d8312cf7a
Quinn Lanphere <qlanphere@yahoo.com>
2021-10-28 17:52:56 +0100

    fixed crash at end of game

--- src/pages/EndGame/index.js
+++ src/pages/EndGame/index.js
@@ -1,73 +1,100 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useGameContext } from "../../contexts/gameContext";
 import { useAuthContext } from "../../contexts/auth";
-import {Bar} from 'react-chartjs-2'
+import { Bar } from 'react-chartjs-2'
 const EndGame = () => {
 
-    const {emails, score} = useGameContext()
-    const {currentUser} = useAuthContext()
+    const { emails, score } = useGameContext()
+    const { currentUser } = useAuthContext()
     // const [finalScores, setFinalScores] = useState([])
     // const [userNames, setUserNames] = useState([])
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [patching, setPatching] = useState(true)
+    const [retrieving, setRetrieving] = useState(true)
+    const [gameScore, setGameScore] = useState([])
+    const [gameUsers, setGameUsers] = useState([])
 
-    const filteredEmails = emails.filter((value, index, array) =>array.indexOf(value) === index
+    const filteredEmails = emails.filter((value, index, array) => array.indexOf(value) === index
     );
     const emailString = filteredEmails.join('*')
 
+    let finalScores
+    let userNames
+
 
     useEffect(() => {
-    
-    console.log(score)
-    const sendScore = {
-        game_score: score
-    }
-    let options = {
-        method: 'PATCH',
-        
+
+        console.log(score)
+        const sendScore = {
+            game_score: score
+        }
+        let options = {
+            method: 'PATCH',
+
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: localStorage.getItem("token")
+            },
+            body: JSON.stringify(sendScore),
+        }
+
+        const modify = async () => {
+            let data
+            const response = await fetch(`http://localhost:3000/user/${currentUser.email}`, options)
+            data = await response.json()
+            if(data) {
+            setPatching(false)
+            }
+            console.log(data)
+        }
+
+        modify()
+
+
+    }, [])
+
+    let options2 = {
+        method: 'GET',
         headers: {
-            'Content-Type': 'application/json',
             authorization: localStorage.getItem("token")
-        },
-        body: JSON.stringify(sendScore),
+        }
     }
 
-    const modify = async () => {
-       await fetch(`http://localhost:3000/user/${currentUser.email}`, options)
-    //    const data = await response.json()
-    //    console.log(data)
+    async function retrieve() {
+        let response = await fetch(`http://localhost:3000/user/${emailString}`, options2)
+        let scores = await response.json()
+        setData(scores)
+        setRetrieving(false)
+        // let gameScores = gameUsers.map(user => user.last_score)
+        // let roomUsers = gameUsers.map(user => user.username)
+
+        // setFinalScores(gameScores)
+        // setUserNames(roomUsers)
+        //return scores
     }
-
-    modify()
-
-
-    retrieve()
-
     
 
-}, [])
+    useEffect(() => {
+        if (!patching){
+        retrieve()
+        }
+        console.log(patching)
+    
+    }, [patching])
 
-let options2 = {
-    headers: {
-        'Content-Type': 'application/json',
-        authorization: localStorage.getItem("token")
-    }
-}
 
-async function retrieve () {
-    let response = await fetch(`http://localhost:3000/user/${emailString}`, options2)
-    let scores = await response.json()
-    setData(scores)
-    // let gameScores = gameUsers.map(user => user.last_score)
-    // let roomUsers = gameUsers.map(user => user.username)
+    useEffect(() => {
+        console.log(data)
+        finalScores = data.map(user => user.last_score)
+        userNames = data.map(user => user.username)
+        setGameScore(finalScores)
+        setGameUsers(userNames)
+    }, [retrieving])
 
-    // setFinalScores(gameScores)
-    // setUserNames(roomUsers)
-    //return scores
-}
-console.log(data)
-let finalScores =  data.map(user => user.last_score)
-let userNames = data.map(user => user.username)
-
+    useEffect(() => {
+        setLoading(false)
+    }, [userNames])
 
 
     return (
@@ -77,34 +104,35 @@ let userNames = data.map(user => user.username)
             <div>Game over!</div>
 
             <p>Final Score: {score}</p>
+            <p>{loading ? "Loading" : " "}</p>
 
             <Bar
-            data = {{
-                labels:userNames,
-                datasets: [
-                    {
-                    label: 'Games Played',
-                    data:finalScores,
-                    backgroundColor: ['rgba(255, 99, 132, 0.2)'],
-                    borderColor: ['rgba(255, 99, 132, 1'],
-                    borderWidth: 1
-                }
-                
-                ]
-            }}
-           options={{
-               maintainAspectRatio: true,
-            //    scales: {
-            //        yAxes: [
-            //            {ticks: {
-            //                beginAtZero: true,
-            //            }}
-            //        ]
-            //    }
-        }} />
+                data={{
+                    labels: gameUsers,
+                    datasets: [
+                        {
+                            label: 'Games Played',
+                            data: gameScore,
+                            backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+                            borderColor: ['rgba(255, 99, 132, 1'],
+                            borderWidth: 1
+                        }
 
-        </>
+                    ]
+                }}
+                options={{
+                    maintainAspectRatio: true,
+                    //    scales: {
+                    //        yAxes: [
+                    //            {ticks: {
+                    //                beginAtZero: true,
+                    //            }}
+                    //        ]
+                    //    }
+                }} />
+
+            </>
     )
 }
 
-export default EndGame
+            export default EndGame
