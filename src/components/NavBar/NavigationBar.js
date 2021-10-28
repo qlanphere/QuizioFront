80993427ff3fc94550881ef392c27d628a0d3209
mussb00 <berhanem2000@gmail.com>
2021-10-28 18:54:59 +0100

    home page styling

--- src/components/NavBar/NavigationBar.js
+++ src/components/NavBar/NavigationBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Nav, Navbar, Container } from 'react-bootstrap'
 import { useGameContext } from '../../contexts/gameContext'
 import { useAuthContext } from '../../contexts/auth'
+import QuizIcon from '@mui/icons-material/Quiz';
 const NavigationBar = () => {
     const {currentUser, logout} = useAuthContext()
     console.log(currentUser)
@@ -9,7 +10,7 @@ const NavigationBar = () => {
             <Navbar bg="primary" variant="dark">
                 <Container>
                     <Navbar.Brand href="/">
-                        {/* <img src={} width="" height=""/> */}
+                        <QuizIcon/>
                     </Navbar.Brand>
                     <Navbar.Brand href="/">
                         Quizio
@@ -19,8 +20,7 @@ const NavigationBar = () => {
                         {!currentUser && <Nav.Link href="/register">Register</Nav.Link>}
                         {!currentUser && <Nav.Link href="/login">Login</Nav.Link>}
                         {currentUser && <Nav.Link href = "/room" >Join</Nav.Link>}
-                        {currentUser && <Nav.Link href="/" onClick={logout}>Logout</Nav.Link>}
-                        
+                        {currentUser && <Nav.Link href="/" onClick={logout}>Logout</Nav.Link>} 
                     </Nav>
                 </Container>
             </Navbar>
