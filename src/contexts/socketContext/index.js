20c9afe3ee8c38193bf9ddd26b2126d53a9ab7a8
Quinn Lanphere <qlanphere@yahoo.com>
2021-10-29 10:20:58 +0100

    port change

--- src/contexts/socketContext/index.js
+++ src/contexts/socketContext/index.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
 import React from 'react'
 
-export const socket = io('https://quizioback.herokuapp.com', { transports: ['polling'] });
+export const socket = io('https://quizioback.herokuapp.com:8080', { transports: ['polling'] });
 export const SocketContext = React.createContext()
